cf0c3d6daeb677b93679ba48c66ab766ff8e7420
subbu-yadhati <subbuyadhati@gmail.com>
2025-10-28 14:41:31 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express()
+const dotenv=require('dotenv')
+const cors=require('cors')
+
+
+dotenv.config()
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("hello ");
+})
+
+
+module.exports=app
